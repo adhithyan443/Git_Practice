bf361ce5423d80f9cbccbe021ef6e6d54fec04b9
adhithyan443 <adhithyanr329@gmail.com>
2026-06-02 12:24:41 +0530

    Restarted

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import "fmt"
+func main(){
 
-func main() {
-	fmt.Println(Add(5, 3))
-	fmt.Println(Substract(5, 3))
-	fmt.Println(Multiply(5, 3))
-}
-
-func Add(a, b int) int {
-	return a + b
-}
-
-func Substract(a, b int) int {
-	return a - b
-}
-
-func Multiply(a, b int) int {
-	return a * b
-}
-func Divide(a, b int) int {
-	return a / b
 }
